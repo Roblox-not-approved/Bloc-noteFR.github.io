131cfe8dd66665013392fa33a023f7b4b087a461
Roblox-not-approved <MrFirefoxYTB_off@outlook.com>
2024-11-23 21:32:56 +0100

    Delete bloc note directory

--- bloc note/animations.js
+++
@@ -1,3 +0,0 @@
-gsap.from('.animate-title', { duration: 1, y: -50, opacity: 0, ease: 'bounce' });
-gsap.from('.btn-inscription', { duration: 1.5, x: -200, opacity: 0, delay: 0.5, ease: 'elastic' });
-gsap.from('.btn-connexion', { duration: 1.5, x: 200, opacity: 0, delay: 0.5, ease: 'elastic' });
